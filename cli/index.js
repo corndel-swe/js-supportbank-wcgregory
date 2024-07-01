b83d5d423b333632d6d1035e1c7217263e16bff2
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-07-01 11:27:39 +0000

    day1 workshop split bill

--- cli/index.js
+++ cli/index.js
@@ -1,9 +1,11 @@
 // cli/index.js
 import { program } from 'commander'
 import transactionController from './transaction.js'
+import splitBillController from './splitBill.js'
 
-program.version('0.1.0').description('SupportBank')
+program.version('0.1.1').description('SupportBank')
 
 program.addCommand(transactionController)
+program.addCommand(splitBillController)
 
 program.parse(process.argv)
