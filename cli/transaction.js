b8a04eeaa14fbeae65df01676d48cc96e1a29f96
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-07-01 10:35:14 +0000

    added datetime to transaction

--- cli/transaction.js
+++ cli/transaction.js
@@ -6,7 +6,8 @@ transactionController
   .command('log <from> <to> <amount>')
   .description('Log transaction data to the console')
   .action((from, to, amount) => {
-    console.log(`${from} ${to} ${amount}`)
+    const transactionTime = new Date()
+    console.log(`At ${transactionTime.toDateString()}, ${from} sent ${to} ${amount}`)
   })
 
 export default transactionController
