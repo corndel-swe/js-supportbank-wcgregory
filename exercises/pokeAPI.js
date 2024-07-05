91a8be031b7febaabd99f695a2e22c876a7a1834
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-07-05 16:20:19 +0000

    pokeman tidy up

--- exercises/pokeAPI.js
+++ exercises/pokeAPI.js
@@ -1,6 +1,14 @@
 export async function getPokemonList() {
   // TODO
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+  let response = await fetch('https://pokeapi.co/api/v2/pokemon')
   const jsonOutput = await response.json()
   return jsonOutput.results.map(pokemonNames => pokemonNames.name)
 }
+
+export async function getFullPokemonList() {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=60&offset=60')
+  const jsonOutput = await response.json()
+  return jsonOutput.results.map(pokemonNames => pokemonNames.name)
+}
+
+// console.log(await getFullPokemonList(true))
